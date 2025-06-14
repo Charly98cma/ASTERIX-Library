a4ff40cc13980d8a78842aa1ab2981faad7400f8
Charly98cma <1998cma@gmail.com>
2025-06-14 17:40:01 +0000

    Category 21 / Item 008 - Aircraft Operational Status

---
+++ include/Categories/cat021/cat021_item008.h
@@ -0,0 +1,108 @@
+/**
+ * @file cat021_item008.h
+ * @brief Definition of item 008 of CAT 021, and related functions and values
+ */
+
+#ifndef cat021_item008_H
+#define cat021_item008_H
+
+#include <stdint.h>
+#include "visibility.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*******************************************************************************
+ * Macros
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Structures and Types
+ ******************************************************************************/
+
+/**
+ * @typedef cat021_item008
+ * @brief Category 21 / Item 008 - Aircraft Operational Status
+ * 
+ * Identification of the operational services available in the
+ * aircraft while airborne
+ */
+typedef struct cat021_item008 {
+    union {
+        /// @brief Raw octet as received (recommended for portable access)
+        uint8_t raw;
+
+        /**
+         * @note Bit-field layout is compiler and endianness dependent.
+         * Use raw field and provided macros for portable access.
+         */
+ 
+        /// @brief Bit-field access (might be non-portable, use with caution)
+        struct {
+            /** @brief TCAS Resolution Advisory active
+             * 
+             * = 0 not active |
+             * = 1 active
+             */
+            uint8_t RA      :1;
+            /** @brief Target Trajectory Change Report Capability
+             * 
+             * = 0 no capability for Trajectory Change Reports |
+             * = 1 support for TC+0 reports only |
+             * = 2 support for multiple TC reports |
+             * = 3 reserved
+             */
+            uint8_t TC      :2;
+            /** @brief Target State Report Capability
+             * 
+             * = 0 no capability to support Target State Reports |
+             * = 1 capable of supporting Target State Reports
+             */
+            uint8_t TS      :1;
+            /** @brief Air-Referenced Velocity Report Capability
+             * 
+             * = 0 no capability to generate ARV-reports |
+             * = 1 capable of generate ARV-reports */
+            uint8_t ARV     :1;
+            /** @brief Cockpit Display of Traffic Information airborne
+             * 
+             * = 0 CDTI not operational |
+             * = 1 CDTI operational
+             */
+            uint8_t CDTI    :1;
+            /** @brief Not TCAS System Status
+             * 
+             * = 0 NTCAS operational |
+             * = 1 NTCAS not operational
+             */
+            uint8_t NTCAS    :1;
+            /** @brief Single Antenna
+             * 
+             * = 0 Antenna Diversity |
+             * = 1 Single Antenna only
+             */
+            uint8_t SA      :1;
+        };
+    };
+} cat021_item008;
+
+/*******************************************************************************
+ * Function headers
+ ******************************************************************************/
+
+ASTERIX_API uint8_t get_cat021_item008_RA(const cat021_item008 * item);
+ASTERIX_API uint8_t get_cat021_item008_TC(const cat021_item008 * item);
+ASTERIX_API uint8_t get_cat021_item008_TS(const cat021_item008 * item);
+ASTERIX_API uint8_t get_cat021_item008_ARV(const cat021_item008 * item);
+ASTERIX_API uint8_t get_cat021_item008_CDTI(const cat021_item008 * item);
+ASTERIX_API uint8_t get_cat021_item008_NTCAS(const cat021_item008 * item);
+ASTERIX_API uint8_t get_cat021_item008_SA(const cat021_item008 * item);
+
+ASTERIX_API void print_cat021_item008(const cat021_item008 *item);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* cat021_item008_H */
---
+++ src/Categories/cat021/cat021_item008.c
@@ -0,0 +1,127 @@
+/**
+ * @file cat021_item008.c
+ * @brief Implementation of the Category 21 Item 008 functions
+ */
+
+#include "cat021_item008.h"
+#include "constants.h"
+
+/*******************************************************************************
+ * Getters
+ ******************************************************************************/
+
+/**
+ * @brief Get the TCAS Resolution Advisory status (RA) from I021/008.
+ * 
+ * Portable access to the RA bit, independent of compiler and endianness.
+ * 
+ * @param item Pointer to cat021_item008 structure.
+ * @return uint8_t Value of RA (0: not active, 1: active)
+ */
+uint8_t get_cat021_item008_RA(const cat021_item008 * item)
+{
+    return (((item)->raw >> 7) & MASK_01_BITS);
+}
+
+/**
+ * @brief Get the Target Trajectory Change Report Capability (TC) from I021/008.
+ * 
+ * Portable access to the TC bit, independent of compiler and endianness.
+ * 
+ * @param item Pointer to cat021_item008 structure.
+ * @return uint8_t Value of TC (0: no capability, 1: TC+0 reports only, 2: multiple TC reports, 3: reserved)
+ */
+uint8_t get_cat021_item008_TC(const cat021_item008 * item)
+{
+    return (((item)->raw >> 5) & MASK_02_BITS);
+}
+
+/**
+ * @brief Get the Target State Report Capability (TS) from I021/008.
+ * 
+ * Portable access to the TS bit, independent of compiler and endianness.
+ * 
+ * @param item Pointer to cat021_item008 structure.
+ * @return uint8_t Value of TS (0: no capability, 1: capability supported)
+ */
+uint8_t get_cat021_item008_TS(const cat021_item008 * item)
+{
+    return (((item)->raw >> 4) & MASK_01_BITS);
+}
+
+/**
+ * @brief Get the Air-Referenced Velocity Report Capability (ARV) from I021/008.
+ * 
+ * Portable access to the ARV bit, independent of compiler and endianness.
+ * 
+ * @param item Pointer to cat021_item008 structure.
+ * @return uint8_t Value of ARV (0: no capability. 1: capability supported)
+ */
+uint8_t get_cat021_item008_ARV(const cat021_item008 * item)
+{
+    return (((item)->raw >> 3) & MASK_01_BITS);
+}
+
+/**
+ * @brief Get the Cockpit Display of Traffic Information airborne (CDTI) from I021/008.
+ * 
+ * Portable access to the CDTI bit, independent of compiler and endianness.
+ * 
+ * @param item Pointer to cat021_item008 structure.
+ * @return uint8_t Value of CDTI (0: not operational, 1: operational)
+ */
+uint8_t get_cat021_item008_CDTI(const cat021_item008 * item)
+{
+    return (((item)->raw >> 2) & MASK_01_BITS);
+}
+
+/**
+ * @brief Get the Not TCAS System Status (NTCAS) from I021/008.
+ * 
+ * Portable access to the NTCAS bit, independent of compiler and endianness.
+ * 
+ * @param item Pointer to cat021_item008 structure.
+ * @return uint8_t Value of NTCAS (0: operational, 1: not operational)
+ */
+uint8_t get_cat021_item008_NTCAS(const cat021_item008 * item)
+{
+    return (((item)->raw >> 1) & MASK_01_BITS);
+}
+
+/**
+ * @brief Get the Single Antenna (SA) from I021/008.
+ * 
+ * Portable access to the SA bit, independent of compiler and endianness.
+ * 
+ * @param item Pointer to cat021_item008 structure.
+ * @return uint8_t Value of SA (0: antenna diversity, 1: single antenna only)
+ */
+uint8_t get_cat021_item008_SA(const cat021_item008 * item)
+{
+    return (((item)->raw) & MASK_01_BITS);
+}
+
+/*******************************************************************************
+ * Other Functions
+ ******************************************************************************/
+
+/**
+ * @brief Print the contents of CAT 021 / Item 008.
+ *
+ * This function prints the values of the main byte.
+ * It is useful for debugging and inspection.
+ *
+ * @param item Pointer to a cat021_item008 structure.
+ */
+void print_cat021_item008(const cat021_item008 *item)
+{
+    printf("CAT021 / Item 008 - Aircraft Operational Status\n");
+    printf("  RA: %d\n", get_cat021_item008_RA(item));
+    printf("  TC: %d\n", get_cat021_item008_TC(item));
+    printf("  TS: %d\n", get_cat021_item008_TS(item));
+    printf("  ARV: %d\n", get_cat021_item008_ARV(item));
+    printf("  CDTI: %d\n", get_cat021_item008_CDTI(item));
+    printf("  NTCAS: %d\n", get_cat021_item008_NTCAS(item));
+    printf("  SA: %d\n\n", get_cat021_item008_SA(item));
+}
+
